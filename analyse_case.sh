e7899214e63eb5d568a4606f79232f24c7d839ea
Matteo Marini <chuckberry26@gmail.com>
2010-08-26 14:50:19 +0200

    Add support to analyse_case.sh to use gzipped data files

--- analyse_case.sh
+++ analyse_case.sh
@@ -24,13 +24,19 @@ LOW_SAM="$1"
 UP_SAM="$2"
 LOW_LAT="$3"
 UP_LAT="$4"
-DATA_FILE="$5"
+DATA_FILE_COMPRESSED="$5"
 TRACE_FILE_COMPRESSED="$6"
 VCD_FOLDER="$7"
 SUFFIX_FILE="$8"
 
 if [ $# -lt 8 ]; then
 	print_usage
+	exit 1;
+fi
+
+DATA_FILE=`basename $DATA_FILE_COMPRESSED .gz`
+if [[ ! -f $DATA_FILE_COMPRESSED && ! -f $DATA_FILE ]]; then
+	echo "data file not exists"
 	exit 1;
 fi
 
@@ -41,7 +47,7 @@ fi
 
 if [ $UP_SAM == "def" ]; then
 	echo "default upper bound length of file"
-	UP_SAM=`cat $DATA_FILE | grep -v "#" | wc -l`
+	UP_SAM=`zcat $DATA_FILE_COMPRESSED | grep -v "#" | wc -l`
 fi
 
 if [ x$LOW_LAT == "x" ]; then
@@ -82,8 +88,31 @@ if [ ! -f $TRACE_FILE ]; then
 	exit 1;
 fi
 
-find_sample.sh $LOW_SAM $UP_SAM $LOW_LAT $UP_LAT $DATA_FILE $TRACE_FILE 
+echo "Decompressing data file ..."
+if [ -f $DATA_FILE_COMPRESSED ]; then
+	gunzip $DATA_FILE_COMPRESSED
+fi
 
+TS=`awk -v "low_sam=$LOW_SAM" -v "up_sam=$UP_SAM" -v "low_lat=$LOW_LAT" -v "up_lat=$UP_LAT" '
+
+	NR < low_sam {
+		next;
+	}
+	
+	NR > up_sam {
+		next;
+	}
+
+	NR > low_sam && NR < up_sam {
+		if ($1 > low_lat && $1 < up_lat) {
+			print  $1 " " $2	
+		}
+	}
+' $DATA_FILE | awk '{print $2}'` 
+
+for i in $TS; do
+	ts2gtkw.sh $TRACE_FILE $i
+done
 # now see vcd file
 
 
